7676fce89d6c37a6a4337851eaab54093d319f4e
Nico De Witte <nico.dewitte@vives.be>
2019-08-27 10:57:57 +0200

    Add answer class

---
+++ Examinator/Answer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examinator
+{
+    public class Answer
+    {
+        public Answer(string text)
+            : this(text, false)
+        {
+        }
+
+        public Answer(string text, bool isCorrect)
+        {
+            Text = text;
+            IsCorrect = isCorrect;
+        }
+
+        private bool isCorrect;
+        public bool IsCorrect
+        {
+            get { return isCorrect; }
+            set { isCorrect = value; }
+        }
+
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+    }
+}
