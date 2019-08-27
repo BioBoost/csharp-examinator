99e7a2a595f4809d41dc4bacc672ae8a0cb71176
Nico De Witte <nico.dewitte@vives.be>
2019-08-27 10:16:02 +0200

    Add Fake Database class

---
+++ Examinator/FakeDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examinator
+{
+    public class FakeDatabase : Database
+    {
+        public override List<string> GetAllCourses()
+        {
+            return new List<string>
+            {
+                "Object Oriented Programming",
+                "Introdution to Programmin",
+                "IoT Devices"
+            };
+        }
+    }
+}
