b06c1144d16c201a8bbbb9d98141aa4a6de6e05f
Nico De Witte <nico.dewitte@vives.be>
2019-08-27 10:58:13 +0200

    Add question class

---
+++ Examinator/Question.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examinator
+{
+    public class Question
+    {
+        public Question(string text)
+        {
+            Text = text;
+        }
+
+        public Question(int id, string text)
+        {
+            Id = id;
+            Text = text;
+        }
+
+        public void AddAnswer(Answer answer)
+        {
+            answers.Add(answer);
+        }
+
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        private List<Answer> answers = new List<Answer>();
+        public List<Answer> Answers
+        {
+            get { return answers; }
+            set { answers = value; }
+        }
+
+        private int id = -1;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+}
