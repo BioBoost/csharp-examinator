af1fd98f4bd4d43b2eec48e6e662577ea6bf4bbc
Nico De Witte <nico.dewitte@vives.be>
2019-08-27 12:28:08 +0200

    Add classes to generate exams

---
+++ Examinator/Exam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examinator
+{
+    public abstract class Exam
+    {
+        public List<Question> Questions { get; set; }
+        public Course Course { get; set; }
+
+        public abstract void Generate();
+    }
+}
